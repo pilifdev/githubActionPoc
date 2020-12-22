698816c3945406915a45711be4518e869e7bc399
Philippe Brossier <Philippe.Brossier@os.uk>
2020-12-22 14:04:58 +0000

    simplifying the script

--- .github/actions/poc-action/convertToHtml.sh
+++ .github/actions/poc-action/convertToHtml.sh
@@ -1,7 +1,4 @@
 #!/bin/sh -l
 
 # You could also add the option: -c style.css
-pandoc -f markdown -t html5 -o termsAndConditions.html /termsAndConditions.md
-
-time=$(date)
-echo "::set-output name=time::$time"
+pandoc -f markdown -t html5 -o /termsAndConditions.html /termsAndConditions.md
